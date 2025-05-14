c7d5f809ace6fe619a255f67b04fc468700c37b4
김혜림 <hyerim@gimhyelim-ui-MacBookAir.local>
2025-05-14 21:20:12 +0900

    모든 파일 삭제

--- src/main/java/com/recap/domain/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.recap.domain.user.repository;
-
-import com.recap.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
